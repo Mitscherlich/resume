c1e4ccc4aaf1fc85e96494fad8d68065f51e9275
Mitscherlich <mitscherlich36@gmail.com>
2019-02-11 12:48:32 +0800

    :lipstick: Update style
    1) Add a "fork me" strip;
    2) Add bottom padding of scroller content;
    3) Fix actionbar click and touch event: now touch on screen will toggle popup menu correctly.

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -1,12 +1,38 @@
 import m from 'mithril'
 
+// const $ = selector => document.querySelector(selector)
+const $$ = selector => Array.prototype.slice.call(document.querySelectorAll(selector))
+
+function matches(el, selector) {
+  return (
+    el.matches ||
+    el.matchesSelector ||
+    el.msMatchesSelector ||
+    el.mozMatchesSelector ||
+    el.webkitMatchesSelector ||
+    el.oMatchesSelector
+  ).call(el, selector)
+}
+
+const state = {
+  shouldShowPopup: false,
+  togglePopup() {
+    state.shouldShowPopup = !state.shouldShowPopup
+    m.redraw()
+  }
+}
+
 export default {
-  oninit(vnode) {
-    vnode.state = {
-      shouldShowPopup: false,
-      togglePopup() {
-        vnode.state.shouldShowPopup = !vnode.state.shouldShowPopup
-        m.redraw()
+  oncreate() {
+    document.onmouseup = document.ontouchend = function(e) {
+      if (
+        state.shouldShowPopup &&
+        !matches(e.target, '.action:first-child') &&
+        !matches(e.target, '.btn-popup') &&
+        !matches(e.target, 'li.item') &&
+        $$('.action:first-child, .btn-popup, li.item').indexOf(e.target) < 0
+      ) {
+        state.togglePopup()
       }
     }
   },
@@ -14,27 +40,29 @@ export default {
     return (
       <footer class="actionbar">
         <ul class="actions">
-          <li class="action" onclick={vnode.state.togglePopup}>
-            <a href="#">{vnode.state.shouldShowPopup ? '▼' : '▲'} 关于我</a>
-            <ul class={['popup', vnode.state.shouldShowPopup ? 'active' : ''].join(' ')}>
+          <li class="action" onclick={state.togglePopup}>
+            <a class="btn-popup" href="#">
+              {state.shouldShowPopup ? '▼' : '▲'} 关于我
+            </a>
+            <ul class={['popup', state.shouldShowPopup ? 'active' : ''].join(' ')}>
               <li
                 class="item"
                 action={'edu'}
-                onclick={m.withAttr('action', vnode.attrs.addMessage)}
+                onclick={m.withAttr('action', vnode.attrs.sendQuestion)}
               >
                 教育经历
               </li>
               <li
                 class="item"
                 action={'school'}
-                onclick={m.withAttr('action', vnode.attrs.addMessage)}
+                onclick={m.withAttr('action', vnode.attrs.sendQuestion)}
               >
                 校园经历
               </li>
               <li
                 class="item"
                 action={'awards'}
-                onclick={m.withAttr('action', vnode.attrs.addMessage)}
+                onclick={m.withAttr('action', vnode.attrs.sendQuestion)}
               >
                 获得成就
               </li>
@@ -43,14 +71,14 @@ export default {
           <li
             class="action"
             action={'skills'}
-            onclick={m.withAttr('action', vnode.attrs.addMessage)}
+            onclick={m.withAttr('action', vnode.attrs.sendQuestion)}
           >
             <a href="#">专业技能</a>
           </li>
           <li
             class="action"
             action={'habits'}
-            onclick={m.withAttr('action', vnode.attrs.addMessage)}
+            onclick={m.withAttr('action', vnode.attrs.sendQuestion)}
           >
             <a href="#">兴趣爱好</a>
           </li>
